6895402b52317b69dacb5fc343566643273e3eb7
Jonathan Rim <jonrim@umich.edu>
2017-09-05 18:40:56 -0400

    trying to get checkbox hack to work

--- client/src/components/Landing/component.js
+++ client/src/components/Landing/component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Label, Grid, Segment, Image } from 'semantic-ui-react';
+import { Button, Label, Grid, Segment } from 'semantic-ui-react';
 
 export default class Landing extends Component {
   constructor(props) {
@@ -28,25 +28,24 @@ export default class Landing extends Component {
           </div>
         </div>
         <Grid columns={2} stackable divided doubling verticalAlign='middle'>
-          <Grid.Column id="cloud-partners">
-            <Image
-              fluid
-              label={{ color: 'blue', corner: 'left' }}
-            />
-            <text className='corner-label-text'>freelance</text>
-            <div className='work-wrapper'>
-              <img src={require('lib/images/top.png')}></img>
-              <div className='info'>
-                <h4>Freelance</h4>
-                <h4>Full Stack Engineer</h4>
-                <p className="subtitle">July 2017 - Dec 2017</p>
+          <Grid.Column id='cloud-partners'>
+            <input type='checkbox' id='cloud-partners-btn' className='col-btns' />
+            <label for='cloud-partners-btn'>
+              <Label floating color='blue'>Freelance</Label>
+              <div className='work-wrapper'>
+                <img src={require('lib/images/top.png')}></img>
+                <div className='info'>
+                  <h4>Full Stack Engineer</h4>
+                  <p className="subtitle"><span>July 2017</span> - <span>Dec 2017</span></p>
+                </div>
+                <div className="description">
+                  <p>Currently building the entire client site of Cloud Partners' future platform.</p>
+                  <p>Used for creating and evaluating assessments of interviewees
+                  for companies like Goldman Sachs.</p>
+                  <Button />
+                </div>
               </div>
-              <p className="description">
-                Currently building the entire client site of Cloud Partners' future platform.
-                Used for creating and evaluating assessments of interviewees
-                for companies like Goldman Sachs.
-              </p>
-            </div>
+            </label>
           </Grid.Column>
           asd
           <Grid.Column>
