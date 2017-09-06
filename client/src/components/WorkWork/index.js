f2d282ad9e6c1acbccb3978b0e4e4b9232f0c597
Jonathan Rim <jonrim@umich.edu>
2017-09-06 18:41:41 -0400

    test

--- client/src/components/WorkWork/index.js
+++ client/src/components/WorkWork/index.js
@@ -4,25 +4,25 @@ import { Button, Label, Grid } from 'semantic-ui-react';
 import './index.scss';
 
 export default class WorkWork extends Component {
-	render() {
-		return (
-			<Grid.Column className='grid-col' id='col-4'>
-				<div className='col-wrapper'>
-					<div className='work' id='workwork'>
-					  <Label floating color='red'>Technical</Label>
-					  <div className='work-wrapper'>
+  render() {
+    return (
+      <Grid.Column className='grid-col' id='col-4'>
+        <div className='col-wrapper'>
+          <div className='work' id='workwork'>
+            <Label floating color='red'>Technical</Label>
+            <div className='work-wrapper'>
 
-					    <div className='info'>
-					      <h2>WORKWORK.JS</h2>
-					      <p className="subtitle">JAVASCRIPT LIBRARY THAT CAN RUN COMPUTATION-HEAVY TASKS CONCURRENTLY IN THE BROWSER</p>
-					    </div>
-					    <div className='description'>
-
-					    </div>
-					  </div>
-					</div>
-				</div>
-			</Grid.Column>
-		)
-	}
+              <div className='info'>
+                <h2>WORKWORK.JS</h2>
+                <p className="subtitle">JAVASCRIPT LIBRARY THAT CAN RUN COMPUTATION-HEAVY TASKS CONCURRENTLY IN THE BROWSER</p>
+              </div>
+              <div className='description'>
+                <Button>More Info</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Grid.Column>
+    )
+  }
 }
