c72215021a7b46337e81a778b2c1e8419fe12e38
Jonathan Rim <jonrim@umich.edu>
2017-09-08 20:32:45 -0400

    fixed haru buttons

--- client/src/components/Haru/index.js
+++ client/src/components/Haru/index.js
@@ -4,35 +4,34 @@ import { Button, Grid } from 'semantic-ui-react';
 import './index.scss';
 
 export default class Haru extends Component {
-	render() {
-		return (
-			<Grid.Column className='grid-col' id='col-6'>
-				<div className='col-wrapper'>
-					<div className='work' id='haru'>
-					  <div className='work-wrapper'>
-					    <div className='info'>
-					      <h2>HARU THE SHIBA INU</h2>
-					      <p className="subtitle">A STATIC E-COMMERCE PAGE FOR A YOUTUBE CHANNEL</p>
-					    </div>
-					    <div className='description'>
-				      	<p><a href='//harushop.herokuapp.com' target='_blank'>harushop.herokuapp.com</a></p>
-				      	<p>
-				      		<a
-				      			href='https://www.youtube.com/channel/UCYd6CmhFvvq6yruUBmGXjuA/feed'
-				      			target='_blank'
-				      		>
-				      			YouTube Channel
-				      		</a>
-				      	</p>
-					      <p>Built a static e-commerce page for 28k+ subscribers YouTube channel.</p>
-					      <Button>
-					    		More Info
-					    	</Button>
-					    </div>
-					  </div>
-					</div>
-				</div>
-			</Grid.Column>
-		)
-	}
+  render() {
+    return (
+      <Grid.Column className='grid-col' id='col-6'>
+        <div className='col-wrapper'>
+          <div className='work' id='haru'>
+            <div className='work-wrapper'>
+              <div className='info'>
+                <h2>HARU THE SHIBA INU</h2>
+                <p className="subtitle">A STATIC E-COMMERCE PAGE FOR A YOUTUBE CHANNEL</p>
+              </div>
+              <a href='https://www.youtube.com/channel/UCYd6CmhFvvq6yruUBmGXjuA/feed' target='_blank'>
+                <Button className='website-button youtube-button'>
+                  <i className='fa fa-youtube-square' />
+                </Button>
+              </a>
+              <a href='//harushop.herokuapp.com' target='_blank'>
+                <Button className='website-button' icon='world' content='www' />
+              </a>
+              <div className='description'>
+                <p>Built a static e-commerce page for 28k+ subscribers YouTube channel.</p>
+                <Button>
+                  More Info
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Grid.Column>
+    )
+  }
 }
